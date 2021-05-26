61035b03572af637980e195e09cbde90339f836c
Kevin Le <kevinle108@gmail.com>
2021-05-26 15:21:21 -0400

    working random double dice roll

---
+++ android/app/src/main/kotlin/kevinle/flutter_dice_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package kevinle.flutter_dice_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                  onPressed: (){
+                    print('Left dice pressed');
+                    setState(() {
+                      leftDiceNumber = Random().nextInt(6) + 1;
+                      rightDiceNumber = Random().nextInt(6) + 1;
+                      print('Dice Roll: ${leftDiceNumber + rightDiceNumber}');
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'))
+          ),
+          Expanded(
+              child: TextButton(
+                  onPressed: (){
+                    print('Right dice pressed');
+                    setState(() {
+                      leftDiceNumber = Random().nextInt(6) + 1;
+                      rightDiceNumber = Random().nextInt(6) + 1;
+                      print('Dice Roll: ${leftDiceNumber + rightDiceNumber}');
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'))
+          ),
+        ],
+      ),
+    );
+  }
+}
+
